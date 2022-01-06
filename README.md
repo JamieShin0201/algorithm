90b75f8a682fae4fa57adeda04188bc37a7ea082
JamieShin0201 <jamieshin1450@gmail.com>
2022-01-06 14:39:23 +0900

    Leetcode 209. Minimum Size Subarray Sum

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## Array
 
+- [209. Minimum Size Subarray Sum](https://leetcode.com/problems/minimum-size-subarray-sum/)
 - [283. Move Zeroes](https://leetcode.com/problems/move-zeroes/)
 - [704. Binary Search](https://leetcode.com/problems/binary-search/)
 - [724. Find Pivot Index](https://leetcode.com/problems/find-pivot-index/)
---
+++ src/main/java/array/MiniMumSizeSubarraySum.java
@@ -0,0 +1,26 @@
+package array;
+
+public class MiniMumSizeSubarraySum {
+
+    public static void main(String[] args) {
+        int target = 7;
+        int[] nums = {2, 3, 1, 2, 4, 3};
+        System.out.println(minSubArrayLen(target, nums));
+    }
+
+    public static int minSubArrayLen(int target, int[] nums) {
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        while (right < nums.length) {
+            sum += nums[right++];
+            while (sum >= target) {
+                min = Math.min(min, right - left);
+                sum -= nums[left++];
+            }
+        }
+
+        return min == Integer.MAX_VALUE ? 0 : min;
+    }
+}
