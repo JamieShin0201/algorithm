a2ebf3dfd415ee11f8cd0084a59e2f5f5f70565e
JamieShin0201 <jamieshin1450@gmail.com>
2022-01-26 19:33:24 +0900

    Leetcode 733. Flood Fill - DFS

--- src/main/java/graph/FloodFill.java
+++ src/main/java/graph/FloodFill.java
@@ -4,13 +4,43 @@ import java.util.*;
 
 public class FloodFill {
     public static void main(String[] args) {
-        int[][] image = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
-        floodFill(image, 1, 1, 2);
+        int[][] image = {{0, 0, 0}, {0, 1, 1}};
+        floodFillDFS(image, 1, 1, 1);
         for (int i = 0; i < image.length; i++) {
             for (int j = 0; j < image[0].length; j++) {
                 System.out.print(image[i][j] + " ");
             }
             System.out.println();
+        }
+    }
+
+    public static int[][] floodFillDFS(int[][] image, int sr, int sc, int newColor) {
+        dfs(image, sr, sc, image[sr][sc], newColor);
+        return image;
+    }
+
+    public static void dfs(int[][] image, int sr, int sc, int oldColor, int newColor) {
+        image[sr][sc] = newColor;
+
+        int row = image.length;
+        int column = image[0].length;
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+
+        for (int i = 0; i < 4; i++) {
+            int nx = sr + dx[i];
+            int ny = sc + dy[i];
+
+            if (nx < 0 || ny < 0 || nx >= row || ny >= column) {
+                continue;
+            }
+
+            if (image[nx][ny] != oldColor || image[nx][ny] == newColor) {
+                continue;
+            }
+
+            dfs(image, nx, ny, oldColor, newColor);
         }
     }
 
